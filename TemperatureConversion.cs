d6e06781a49ad12dc2dc086c00547b21af321951
Shashank <shashankpateriya4@gmail.com>
2021-07-12 22:00:58 +0530

    [shashank] Add: Temperature Conversion Program

---
+++ TemperatureConversion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTesting
+{
+    class TemperatureConversion
+    {
+        public static void Conversion()
+        {
+
+            int celsius, faren;
+
+            Console.WriteLine("Enter the Temperature in Celsius(°C) : ");
+            celsius = int.Parse(Console.ReadLine());
+            faren = (celsius * 9) / 5 + 32;
+            Console.WriteLine("0Temperature in Fahrenheit is(°F) : " + faren);
+
+        }
+    }
+}
