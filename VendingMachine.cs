f60e0f9bfb69abbd51a6f2ff44aff46258359536
Shashank <shashankpateriya4@gmail.com>
2021-07-12 21:54:38 +0530

    [shashank] Add: Vending Machine Program

---
+++ VendingMachine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTesting
+{
+    class VendingMachine
+    {
+        public static void PrintingNotes()
+        {
+            Console.WriteLine("enter amount");
+            int number = Convert.ToInt32(Console.ReadLine());
+            MinNotes(number);
+        }
+        private static void MinNotes(int amount)
+        {
+            int[] notes = { 2000, 1000, 500, 100, 50, 20, 10, 5, 2, 1 };
+            int[] notecount = new int[notes.Length];
+
+            for (int i = 0; i < notes.Length; i++)
+            {
+                if (amount >= notes[i])
+                {
+                    notecount[i] = amount / notes[i];
+                    amount = amount - (notecount[i] * notes[i]);
+                    if (amount <= 0)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Currency Count");
+            for (int i = 0; i < notes.Length; i++)
+            {
+                if (notecount[i] != 0)
+                {
+                    Console.WriteLine(notes[i] + " : " + notecount[i]);
+                }
+            }
+        }
+    }
+}
