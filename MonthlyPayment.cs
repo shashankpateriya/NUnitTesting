f0c968b2c5d9cad52098005576b3821a6e155b69
Shashank <shashankpateriya4@gmail.com>
2021-07-12 22:03:15 +0530

    [shashank] Add: Program to pay monthly payment of loan and interest

---
+++ MonthlyPayment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTesting
+{
+    class MonthlyPayment
+    {
+        public static void ReadInput()
+        {
+            double years, p, RateOfIntrest;
+
+            //getting year from user
+            Console.WriteLine("Enter the number of years");
+            years = Convert.ToDouble(Console.ReadLine());
+
+            //Getting Principle amount from user
+            Console.WriteLine("Enter the Principle amount");
+            p = Convert.ToDouble(Console.ReadLine());
+
+            //Getting rate of intrest from user
+            Console.WriteLine("Enter the rate of interest");
+            RateOfIntrest = Convert.ToDouble(Console.ReadLine());
+
+            MonthlyPaymentIs(years, p, RateOfIntrest);
+        }
+        private static void MonthlyPaymentIs(double years, double p, double RateOfIntrest)
+        {
+            double m = 12 * years;
+            double r = RateOfIntrest / (12 * 100);
+            double x = Math.Pow(1 + r, m);
+            double payment = (p * r * x) / (x - 1);
+
+            Console.WriteLine($"Monthly Payment is = {Math.Round(payment)}");
+        }
+    }
+}
