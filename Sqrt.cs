ca9412e5cfae962956011e80e26fa8613edc3b24
Shashank <shashankpateriya4@gmail.com>
2021-07-12 22:04:51 +0530

    [shashank] Add: Program to find Square root using Newtons method

---
+++ Sqrt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTesting
+{
+    class Sqrt
+    {
+        public static void ReadInput()
+        {
+            //Get the input from user
+            Console.WriteLine("Enter the number : ");
+            double c = Convert.ToDouble(Console.ReadLine());
+            NewtonSquareRoot(c);
+        }
+        public static void NewtonSquareRoot(double c)
+        {
+            double t = c;
+            double root = 0;
+            int i = 0;
+            while (true)
+            {
+                root = 0.5 * (t + (c / t));
+                double epsilon = 1e-15;
+                if (Math.Abs(t - c / t) < epsilon * t)
+                {
+                    break;
+                }
+                t = root;
+                i++;
+            }
+            Console.WriteLine(root);
+        }
+
+    }
+}
